c0d33bcdf1480790d5f92751a552354f5b026aec
Kalium1993 <tassio_tln@hotmail.com>
2025-01-17 22:20:07 -0300

    base do projeto

---
+++ src/main/java/br/com/acerta/AcertaApplication.java
@@ -0,0 +1,13 @@
+package br.com.acerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcertaApplication.class, args);
+	}
+
+}
