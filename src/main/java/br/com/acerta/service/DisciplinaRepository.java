0dff0a52a4ccc9bcc990e10266631d6f8bf8b066
Daniela Soares <112218335+DanyelaSoares@users.noreply.github.com>
2025-01-20 19:31:46 -0300

    deletando arquivo repetido

--- src/main/java/br/com/acerta/service/DisciplinaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.acerta.repository;
-
-import br.com.acerta.model.Disciplina;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
-}
