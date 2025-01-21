09280d6b6a5a170a210bba242bfe07755e5f2436
Daniela Soares <112218335+DanyelaSoares@users.noreply.github.com>
2025-01-20 23:05:11 -0300

    DisciplinaRepository e UsuarioDisciplinaRepository

---
+++ src/main/java/br/com/acerta/repository/UsuarioDisciplinaRepository.java
@@ -0,0 +1,7 @@
+package br.com.acerta.repository;
+
+import br.com.acerta.model.UsuarioDisciplina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioDisciplinaRepository extends JpaRepository<UsuarioDisciplina, Long> {
+}
