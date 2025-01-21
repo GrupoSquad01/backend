00d1752c4e4c883426a647f2bb740152dcbda866
Daniela Soares <112218335+DanyelaSoares@users.noreply.github.com>
2025-01-20 23:04:16 -0300

    Delete DisciplinaUsuarioRepository.java

--- src/main/java/br/com/acerta/repository/DisciplinaUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.acerta.repository;
-
-import br.com.acerta.model.DisciplinaUsuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DisciplinaUsuarioRepository extends JpaRepository<DisciplinaUsuario, Long> {
-}
